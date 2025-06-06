474e3a0760a99b7204cfb1b880a6c94d1b8458be
Egbe Michel <egbemichel39@gmail.com>
2025-06-06 19:56:31 +0100

    feat: Add tawk.to

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,19 @@ export default function RootLayout({
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
+
+    <script type="text/javascript">
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/6810f8c4e616e1190d99eaf5/1iq14ngnf';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+
       <body
       >
       <MainLayout>{children}</MainLayout>
